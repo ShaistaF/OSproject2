401b03c50d35d7f505ad885e1e5ea82f9acee7fc
ShaistaF <86759657+ShaistaF@users.noreply.github.com>
2024-04-30 13:41:13 -0500

    Update source.go

--- builtins/source.go
+++ builtins/source.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 )
 
+// Source reads and executes commands from a file in the current shell environment.
 func Source(w io.Writer, args ...string) error {
 	if len(args) < 1 {
 		return fmt.Errorf("usage: source filename")
@@ -19,6 +20,8 @@ func Source(w io.Writer, args ...string) error {
 	}
 	defer file.Close()
 
+	var errors []error
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -29,12 +32,20 @@ func Source(w io.Writer, args ...string) error {
 		}
 		// Execute the shell command from the source file.
 		if err := handleInput(w, line); err != nil {
-			return err
+			errors = append(errors, fmt.Errorf("error executing command: %s", err.Error()))
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		return err
+		errors = append(errors, err)
+	}
+
+	if len(errors) > 0 {
+		var errMsg string
+		for _, err := range errors {
+			errMsg += fmt.Sprintf("%s\n", err.Error())
+		}
+		return fmt.Errorf("encountered %d error(s) while sourcing file:\n%s", len(errors), errMsg)
 	}
 
 	return nil
