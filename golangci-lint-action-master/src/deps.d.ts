6ab3d09ef5313914d0fd6fd3e9aa4845c7ae9fb2
ShaistaF <86759657+ShaistaF@users.noreply.github.com>
2024-05-02 10:43:44 -0500

    Delete golangci-lint-action-master directory

--- golangci-lint-action-master/src/deps.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "cache/lib/restore" {
-  function run(): Promise<void>
-  export default run
-}
-
-declare module "cache/lib/save" {
-  function run(): Promise<void>
-  export default run
-}
--- golangci-lint-action-master/src/post_main.ts
+++
@@ -1,3 +0,0 @@
-import { postRun } from "./run"
-
-postRun()
